8a84c86ff9cc86764fd9a99c3f474562120b27c5
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-05-31 18:30:51 +0900

    Updates

---
+++ precache-manifest.690982a7e9500b6696e1749a5f879cf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c751392a69bb5ca22cc",
+    "url": "/grj_6th_week/static/css/main.28f2e73a.chunk.css"
+  },
+  {
+    "revision": "1c751392a69bb5ca22cc",
+    "url": "/grj_6th_week/static/js/main.1c751392.chunk.js"
+  },
+  {
+    "revision": "9e545d7cacce6086e261",
+    "url": "/grj_6th_week/static/js/1.9e545d7c.chunk.js"
+  },
+  {
+    "revision": "8fc282a7a530261b88e2",
+    "url": "/grj_6th_week/static/js/runtime~main.8fc282a7.js"
+  },
+  {
+    "revision": "3e96df0335ba8b46dfdc2ff57d06b7a3",
+    "url": "/grj_6th_week/index.html"
+  }
+];
